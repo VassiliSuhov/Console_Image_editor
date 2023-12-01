0992e3a9394c4121cad63459494b5df290289e44
Vassili <67841707+SavageDud@users.noreply.github.com>
2023-11-30 21:00:58 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,15 +17,17 @@ using System.Runtime.Intrinsics;
 
 
 
+//example 1
 
+/*
 
-const int FRAMERATE = 10;
+const int FRAMERATE = 60;
 
-const int NOISE_SIZE_X = 8,
-          NOISE_SIZE_Y = 8,
-          IMAGE_DETAIL_X = 4,
-          IMAGE_DETAIL_Y = 4,
-          RANDOM_SEED = 76;
+const int NOISE_SIZE_X = 10,
+          NOISE_SIZE_Y = 10,
+          IMAGE_DETAIL_X = 6,
+          IMAGE_DETAIL_Y = 6,
+          RANDOM_SEED = 36;
 
 
 PerlinNoiseMap perlin_n_map = new PerlinNoiseMap(NOISE_SIZE_X, NOISE_SIZE_Y, IMAGE_DETAIL_X, IMAGE_DETAIL_Y, RANDOM_SEED);
@@ -34,32 +36,81 @@ PerlinNoiseMap perlin_n_map = new PerlinNoiseMap(NOISE_SIZE_X, NOISE_SIZE_Y, IMA
 
 Image_Editing editor = new Image_Editing(FRAMERATE);
 
-
-
-editor.Convert_to_255(perlin_n_map.noise_map);
-editor.Render_255_BW_image(perlin_n_map.noise_map);
-
-
-/*
-const int MAX_FRAME = 1000;
+const int MAX_FRAME = 2000;
 const int window_gap = 50;
 
 
-int image_size = editor.getImageSize(perlin_n_map.noise_map)[0];
+int image_size = editor.GetImageSize(perlin_n_map.noise_map)[0];
 
+const int SPEED = 1;
 
-
-for (int frame_number = 1; frame_number < MAX_FRAME; frame_number++)
+for (int frame_number = 1; frame_number < MAX_FRAME; frame_number += SPEED)
 {
-    Console.Clear();
-    int start = editor.Format_int_to_range(frame_number, 0, image_size);
-    int end = editor.Format_int_to_range(frame_number + window_gap , 0 , image_size);
+   
 
-    editor.Partial_Vertical_display(perlin_n_map.noise_map, start, end , image_size);
+    int start = editor.Format_int_to_range(MAX_FRAME - frame_number, 0, image_size);
+
+
+    int[,] partialmap = editor.CopyPartialVerticalImage(perlin_n_map.noise_map, window_gap, start);
+
+    
+    Console.Clear();
+    editor.Render_255_BW_image(partialmap);
     editor.Frame_Sleep();
 }
 
 
+
 */
-editor = null;
-perlin_n_map = null;
+
+
+
+
+
+
+
+//example 2
+
+/*
+
+const int FRAMERATE = 60;
+
+const int NOISE_SIZE_X = 10,
+          NOISE_SIZE_Y = 10,
+          IMAGE_DETAIL_X = 6,
+          IMAGE_DETAIL_Y = 6,
+          RANDOM_SEED = 36;
+
+
+PerlinNoiseMap perlin_n_map = new PerlinNoiseMap(NOISE_SIZE_X, NOISE_SIZE_Y, IMAGE_DETAIL_X, IMAGE_DETAIL_Y, RANDOM_SEED);
+
+
+
+Image_Editing editor = new Image_Editing(FRAMERATE);
+
+const int MAX_FRAME = 2000;
+const int window_gap = 50;
+
+
+int image_size = editor.GetImageSize(perlin_n_map.noise_map)[0];
+
+const int SPEED = 1;
+
+for (int frame_number = 1; frame_number < MAX_FRAME; frame_number += SPEED)
+{
+   
+
+    int start = editor.Format_int_to_range(MAX_FRAME - frame_number, 0, image_size);
+
+
+    int[,] partialmap = editor.CopyPartialVerticalImage(perlin_n_map.noise_map, window_gap, start);
+
+    editor.FlareShader(partialmap);
+    Console.Clear();
+    editor.Render_255_BW_image(partialmap);
+    editor.Frame_Sleep();
+}
+
+
+
+*/
